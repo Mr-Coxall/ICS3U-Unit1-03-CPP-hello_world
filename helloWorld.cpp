d22bbbf84e0ff3103c81a3b398a99a3ad9c9af60
Patrick Coxall <mr.coxall@mths.ca>
2021-03-02 20:37:38 +0000

    init

---
+++ helloWorld.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sept 2020
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+  
+int main() {
+  std::cout << "Hello, World!";
+}
